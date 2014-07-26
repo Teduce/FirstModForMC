9db5f22b1f71c85adb0ee58860e04abf9818fb9d
Teduce <Sk8dude.OR@gmail.com>
2014-07-25 16:11:25 -0800

    Commiting First Mod

---
+++ src/main/java/com/Teduce/FirstModForMC/FirstModFofMC.java
@@ -0,0 +1,34 @@
+package com.Teduce.FirstModForMC;
+
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "FirstModForMC", name = "First Mod For MC", version = "1.7.10-1.0")
+
+public class FirstModFofMC
+{
+    @Mod.Instance("FirstModForMC")
+    public static FirstModFofMC instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
